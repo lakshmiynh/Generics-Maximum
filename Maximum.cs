29c9d877bfe8361be2c09f3cda6c6ff583061ba5
Lakshmi <lakshmiynh8@gmail.com>
2024-03-22 12:35:00 +0530

    Added:functionality to calculate maximum for more parameters using params

--- Maximum.cs
+++ Maximum.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,18 +9,21 @@ namespace generics
 {
      class Maximum
     {
-        public T Findmax<T>(T num1, T num2, T num3) where T:IComparable
+        public T? Findmax<T>(params T[] values) where T:IComparable
         {
-            T max = num1;
-            if (num2.CompareTo(max) > 0) max = num2; 
-            if (num3.CompareTo(max) > 0) max = num3;
-            return max;
+           List<T> list = new List<T>();
+            foreach (T value in values) 
+            {
+                list.Add(value);
+            }
+            list.Sort();
+            return list.Last();
         }
        
         static void Main(string[] args) 
         {
             Maximum maximum = new Maximum();
-           int integer= maximum.Findmax(1, 2, 3);
+           int integer= maximum.Findmax(165653, 2, 3);
          
             Console.WriteLine(integer);
             float @float= maximum.Findmax(1.4f, 2.1f, 3.87f);
