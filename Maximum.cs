074866117834ccc8ee0aca3b624a9c5eeaa8c51a
Lakshmi <lakshmiynh8@gmail.com>
2024-03-22 12:08:48 +0530

    Added:function to calculate the Maximum in given integers

---
+++ Maximum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generics
+{
+     class Maximum
+    {
+        public int Findmax(int num1, int num2, int num3)
+        {
+            int max = num1;
+            if (num2.CompareTo(max) > 0) max = num2; 
+            if (num3.CompareTo(max) > 0) max = num3;
+            return max;
+        }
+       
+        static void Main(string[] args) 
+        {
+            Maximum integer = new Maximum();
+           int result= integer.Findmax(1, 2, 3);
+            Console.WriteLine(result);
+        }
+    }
+}
